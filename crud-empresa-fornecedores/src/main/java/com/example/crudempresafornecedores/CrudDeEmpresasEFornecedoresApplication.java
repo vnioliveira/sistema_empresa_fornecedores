c8855bbe9d5207df6d104380e9906561fcad827f
Marcos Vinícius <vo7775832@gmail.com>
2023-08-24 18:15:51 -0300

    fix: correcao de projeto maven

---
+++ crud-empresa-fornecedores/src/main/java/com/example/crudempresafornecedores/CrudDeEmpresasEFornecedoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudempresafornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeEmpresasEFornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeEmpresasEFornecedoresApplication.class, args);
+	}
+
+}
