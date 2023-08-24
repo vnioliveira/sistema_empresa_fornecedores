1cfef6efa4592ec598152025d6a54da2ae720019
Marcos Vinícius <vo7775832@gmail.com>
2023-08-24 18:11:37 -0300

    feat: implementação inicial do backend

---
+++ crud-empresa-fornecedores/src/test/java/com/example/crudempresafornecedores/CrudDeEmpresasEFornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudempresafornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeEmpresasEFornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
